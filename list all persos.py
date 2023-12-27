0657ae364c2e4b28841fe575d925a14b9f4784ca
AlexisB <misterbalise2@gmail.com>
2023-12-27 01:13:49 +0100

    add list of cara

---
+++ list all persos.py
@@ -0,0 +1,262 @@
+allCara = [
+    # Cosmic
+    "Adam",  # Adam Warlock
+    "Walker",  # Air-Walker
+    "Angela",  # Angela
+    "Annihilus",  # Annihilus
+    "Black Bolt",  # Black Bolt
+    "Capmcu",  # Captain Marvel
+    "Capm",  # Captain Marvel (Classic)
+    "Carnage",  # Carnage (Pre-Update)
+    "Corvus",  # Corvus Glaive
+    "Cgr",  # Cosmic Ghost Rider
+    "Cull",  # Cull Obsidian
+    "Drax",  # Drax
+    "Galan",  # Galan
+    "Gamora",  # Gamora
+    "Gladiator",  # Gladiator
+    "Gorr",  # Gorr
+    "Groot",  # Groot
+    "Heimdall",  # Heimdall
+    "Hela",  # Hela
+    "Hercules",  # Hercules
+    "Hulkling",  # Hulkling
+    "Hyperion",  # Hyperion
+    "Ikaris",  # Ikaris
+    "Kgroot",  # King Groot
+    "Knull",  # Knull
+    "Maestro",  # Maestro
+    "Medusa",  # Medusa
+    "MissM",  # Ms. Marvel
+    "Kamala",  # Ms. Marvel (Kamala Khan)
+    "Nova",  # Nova
+    "Odin",  # Odin
+    "Phoenix",  # Phoenix
+    "Proxima",  # Proxima Midnight
+    "Redgob",  # Red Goblin
+    "Ronan",  # Ronan
+    "Sersi",  # Sersi
+    "Surfer",  # Silver Surfer
+    "SpiderSymbiote",  # Spider-Man (Symbiote)
+    "Skrull",  # Super-Skrull
+    "Imsup",  # Superior Iron Man
+    "Terrax",  # Terrax
+    "Thanos",  # Thanos
+    "Champion",  # The Champion
+    "Thor",  # Thor
+    "Venom",  # Venom
+    "Vtd",  # Venom the Duck
+    "Venompool",  # Venompool
+    "Aarkus",  # Vision (Aarkus)
+    "Vox",  # Vox
+    # Mutant
+    "Apo",  # Apocalypse
+    "AA",  # Archangel
+    "Beast",  # Beast
+    "Bishop",  # Bishop
+    "Cable",  # Cable
+    "Britain",  # Captain Britain
+    "Colossus",  # Colossus
+    "Cyclopsb",  # Cyclops (Blue Team)
+    "Cyclopsr",  # Cyclops (New Xavier School)
+    "Dani",  # Dani Moonstar
+    "Deadpoolc",  # Deadpool
+    "Deadpoolx",  # Deadpool (X-Force)
+    "Domino",  # Domino
+    "Emma",  # Emma Frost
+    "Gambit",  # Gambit
+    "Goldpool",  # Goldpool
+    "Havok",  # Havok
+    "Iceman",  # Iceman
+    "Jubilee",  # Jubilee
+    "Kitty",  # Kitty Pryde
+    "Redmag",  # Magneto
+    "Whimag",  # Magneto (House of X)
+    "Sinister",  # Mister Sinister
+    "Namor",  # Namor
+    "Diablo",  # Nightcrawler
+    "Logan",  # Old Man Logan
+    "Omega",  # Omega Red
+    "Platpool",  # Platinumpool
+    "Profx",  # Professor X
+    "Psylocke",  # Psylocke
+    "Rogue",  # Rogue
+    "Sabretooth",  # Sabretooth
+    "Sauron",  # Sauron
+    "Storm",  # Storm
+    "Pyramidx",  # Storm (Pyramid X)
+    "Stryfe",  # Stryfe
+    "Sunspot",  # Sunspot
+    "Toad",  # Toad
+    "Wolverine",  # Wolverine
+    "Weaponx",  # Wolverine (Weapon X)
+    "x23",  # Wolverine (X-23)
+    # Mystic
+    "Absman",  # Absorbing Man
+    "Chavez",  # America Chavez
+    "Bwcv",  # Black Widow (Claire Voyant)
+    "Ablo",  # Diablo
+    "Doom",  # Doctor Doom
+    "Strange",  # Doctor Strange
+    "Voodoo",  # Doctor Voodoo
+    "Dormammu",  # Dormammu
+    "Dm",  # Dragon Man
+    "Ebony",  # Ebony Maw
+    "GR",  # Ghost Rider
+    "Guigui",  # Guillotine
+    "Fist",  # Iron Fist
+    "Juggs",  # Juggernaut
+    "Kindred",  # Kindred
+    "Kushala",  # Kushala
+    "Loki",  # Loki
+    "Longshot",  # Longshot
+    "Magik",  # Magik
+    "Mangog",  # Mangog
+    "Man-Thing",  # Man-Thing
+    "Mephisto",  # Mephisto
+    "Mojo",  # Mojo
+    "Mordo",  # Mordo
+    "Morningstar",  # Morningstar
+    "Purgatory",  # Purgatory
+    "Rintrah",  # Rintrah
+    "Sassy",  # Sasquatch
+    "SW",  # Scarlet Witch
+    "SWC",  # Scarlet Witch (Classic)
+    "Sorcersup",  # Sorcerer Supreme
+    "Spidersup",  # Spider-Man (Supreme)
+    "SymSup",  # Symbiote Supreme
+    "Hood",  # The Hood
+    "Jane",  # Thor (Jane Foster)
+    "Tigra",  # Tigra
+    "Werewolf",  # Werewolf By Night
+    "Wiccan",  # Wiccan
+    "Wong",  # Wong
+    # Science
+    "Abo",  # Abomination
+    "Immoabo",  # Abomination (Immortal)
+    "Antman",  # Ant-Man
+    "Antivenom",  # Anti-Venom
+    "Cap",  # Captain America
+    "Capiw",  # Captain America (Infinity War)
+    "Capww2",  # Captain America (WWII)
+    "Cassie",  # Cassie Lang
+    "Electro",  # Electro
+    "Hulk",  # Hulk
+    "Immohulk",  # Hulk (Immortal)
+    "Ragnahulk",  # Hulk (Ragnarok)
+    "Torch",  # Human Torch
+    "Invisible",  # Invisible Woman
+    "Jessica",  # Jessica Jones
+    "Fixit",  # Joe Fixit
+    "Cage",  # Luke Cage
+    "Modok",  # M.O.D.O.K.
+    "Fantastic",  # Mister Fantastic
+    "Negative",  # Mister Negative
+    "Morbius",  # Morbius
+    "Photon",  # Photon
+    "Quake",  # Quake
+    "Quicks",  # Quicksilver
+    "Redguard",  # Red Guardian
+    "Redhulk",  # Red Hulk
+    "Rhino",  # Rhino
+    "Sandman",  # Sandman
+    "Scorpion",  # Scorpion
+    "Sentry",  # Sentry
+    "Shehulk",  # She-Hulk
+    "Silk",  # Silk
+    "Gwen",  # Spider-Gwen
+    "Ham",  # Spider-Ham
+    "Spider",  # Spider-Man (Classic)
+    "Morales",  # Spider-Man (Miles Morales)
+    "Spider99",  # Spider-Man 2099
+    "Spot",  # Spot
+    "Overseer",  # The Overseer
+    "Thing",  # Thing
+    "Titania",  # Titania
+    "Void",  # Void
+    "Wasp",  # Wasp
+    "Yellowjacket",  # Yellowjacket
+    # Skill
+    "Aegon",  # Ægon
+    "Agentvenom",  # Agent Venom
+    "Attuma",  # Attuma
+    "Zemo",  # Baron Zemo
+    "Blackcat",  # Black Cat
+    "Bp",  # Black Panther
+    "Bpcw",  # Black Panther (Civil War)
+    "Bw",  # Black Widow
+    "Bwdo",  # Black Widow (Deadly Origin)
+    "Blade",  # Blade
+    "Chee'ilth",  # Chee'ilth
+    "Crossbones",  # Crossbones
+    "Elsa",  # Elsa Bloodstone
+    "Falcon",  # Falcon
+    "Gwenpool",  # Gwenpool
+    "Hawkeye",  # Hawkeye
+    "Monkey",  # Hit-Monkey
+    "Jabari",  # Jabari Panther
+    "Karnak",  # Karnak
+    "Kate",  # Kate Bishop
+    "Killmonger",  # Killmonger
+    "Kingpin",  # Kingpin
+    "Korg",  # Korg
+    "Kraven",  # Kraven
+    "Mantis",  # Mantis
+    "Masacre",  # Masacre
+    "Misty",  # Misty Knight
+    "Taupe",  # Mole Man
+    "Moonknight",  # Moon Knight
+    "Moondragon",  # Moondragon
+    "Nick",  # Nick Fury
+    "Thrasher",  # Night Thrasher
+    "Punisher",  # Punisher
+    "Ronin",  # Ronin
+    "Shang",  # Shang-Chi
+    "Spiderstealth",  # Spider-Man (Stealth Suit)
+    "Squirrel",  # Squirrel Girl
+    "Taskmaster",  # Taskmaster
+    "Ragnathor",  # Thor (Ragnarok)
+    "Valkyrie",  # Valkyrie
+    # Tech
+    "Antmanf",  # Ant-Man (Future)
+    "Sam",  # Captain America (Sam Wilson)
+    "Cw",  # Civil Warrior
+    "Darkhawk",  # Darkhawk
+    "Octopus",  # Doctor Octopus
+    "Ghost",  # Ghost
+    "Greengob",  # Green Goblin
+    "Guardian",  # Guardian
+    "Guigui99",  # Guillotine 2099
+    "Htd",  # Howard the Duck
+    "Hb",  # Hulkbuster
+    "Im",  # Iron Man
+    "Idoom",  # Iron Man (Infamous)
+    "Imiw",  # Iron Man (Infinity War)
+    "Patriot",  # Iron Patriot
+    "Kang",  # Kang the Conqueror
+    "Lady",  # Lady Deathstrike
+    "Mysterio",  # Mysterio
+    "Nebula",  # Nebula
+    "Nimrod",  # Nimrod
+    "Omega",  # Omega Sentinel
+    "Peni",  # Peni Parker
+    "Psycho",  # Psycho-Man
+    "Punisher99",  # Punisher 2099
+    "Redskull",  # Red Skull
+    "Rocket",  # Rocket Raccoon
+    "Sentinel",  # Sentinel
+    "Shocker",  # Shocker
+    "Shuri",  # Shuri
+    "Centurion",  # Silver Centurion
+    "Spiderstark",  # Spider-Man (Stark Enhanced)
+    "Sl",  # Star-Lord
+    "Ultron",  # Ultron
+    "Ultronc",  # Ultron (Classic)
+    "Vision",  # Vision
+    "Visionaou",  # Vision (Age of Ultron)
+    "Viv",  # Viv Vision
+    "Vulture",  # Vulture
+    "Warmachine",  # War Machine
+    "Warlock",  # Warlock
+    "Yondu",  # Yondu
+]
