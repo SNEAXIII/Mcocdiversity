21a2b06d1bebba3e0418d28b0e3bde0006568525
AlexisB <misterbalise2@gmail.com>
2024-01-04 02:09:57 +0100

    add utilies

---
+++ update G2.py
@@ -0,0 +1,11 @@
+from groups import *
+from connector import *
+from time import time
+debut = time()
+
+groups = Groups()
+groups.doEverything()
+
+connector = Connection()
+connector.updateOneGroup(groups.groups[2],2)
+print(f"Il a fallut {round(time() - debut)} secondes")
